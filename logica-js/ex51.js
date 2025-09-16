35b0121304644619a57a467e9e47211eb76787d7
sammy6911 <dalvinha@gmail.com>
2025-09-15 22:12:57 -0300

    add ex 5.x

---
+++ logica-js/ex51.js
@@ -0,0 +1,7 @@
+let senha
+senha = prompt("digite a senha: ")
+if(senha == "6969"){
+    alert("acesso permitido 😎")
+} else {
+    alert("acesso negado 😒")
+}
---
+++ logica-js/ex52.js
@@ -0,0 +1,10 @@
+let A, B
+A = Number(prompt("digite o primeiro numero: "))
+B = Number(prompt("digite o segundo numero: "))
+if (A > B) {
+    alert ("primeiro ("+A+") é maior que o segundo ("+B+")")}
+    else if (B > A) {
+        alert ("primeiro ("+A+") é menor que o segundo ("+B+")")
+    }else {
+        alert ("os numeros são iguais")
+    }
