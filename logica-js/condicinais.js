2d0d5f8d66b88e58857a8126d0a9e4f69c825f9e
sammy6911 <dalvinha@gmail.com>
2025-09-23 22:07:16 -0300

    af

---
+++ logica-js/condicinais.js
@@ -0,0 +1,28 @@
+//const prompt=require("prompt-sync")()
+//const frequencia=prompt("digite sua frequencia: ")
+//const notaFinal=prompt("digite sua nota final: ")
+//if (notaFinal >= 7 && frequencia >= 8) {
+  //  console.log("aprovado")
+//}   else {  
+  //  console.log("reprovado")}
+  
+
+
+//  const prompt=require("prompt-sync")()
+//  let idade =prompt("digite sua idade: ")
+//  let cnh =prompt("tem cnh? (sim/não) ")
+//   if (idade >= 18 && cnh == "sim") {  
+//      console.log("pode dirigir")}
+//       else {
+//       console.log("não pode dirigir")}
+
+
+const prompt=require("prompt-sync")()
+let senha
+let usuario
+usuario = prompt("digite o usuario: ")
+senha = prompt("digite a senha: ")
+  if(senha == "6969" && usuario == "sammy") {
+    console.log("acesso permitido 😎")
+} else {
+ console.log("acesso negado 😒")}
