5348e9ccc7095d046471d5ec9750033d111e38b9
sammy6911 <dalvinha@gmail.com>
2025-09-29 22:17:36 -0300

    ei

---
+++ repeticao/script.js
@@ -0,0 +1,12 @@
+
+//let acertos = Number(prompt("Quantas perguntas você acertou?"))
+//let valorPorQuestao = 10/20
+//let nota = acertos * valorPorQuestao
+//alert("Sua nota é " + nota)
+
+let contador = 0
+while (contador < 2) {
+   // alert ("contador é " + contador)
+    console.log("contador é " + contador)
+    contador = contador + 1
+}
