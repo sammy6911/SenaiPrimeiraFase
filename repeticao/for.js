83e3ed8f92c53d7e2a90b7d49e8d7b31606935ee
sammy6911 <dalvinha@gmail.com>
2025-10-02 21:28:11 -0300

    eita

---
+++ repeticao/for.js
@@ -0,0 +1,21 @@
+// let i =0 
+// while(i<3){
+//     alert("valor do i: "+ i )
+//     i++
+// }
+// for (let i = 0; i < 3; i++) {
+//     alert("valor do i: "+ i )
+// }
+
+
+// let soma = 0
+// for(let i=1; i<10; i++){
+//     let numero = Number(prompt("Digite a "+ (i+1) +"º nota: "))
+//     soma = soma + numero
+// }
+// alert("A soma das notas é: "+ soma)
+
+
+const nomes = ["ana", "bia", "carlos", "daniel"]
+for(let i=0; i<=4; i++){
+    alert(nomes[i])}
